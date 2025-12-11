0d3d88213eca77584c72072adc3a5ba19a0e8b73
Julius Kirsch <Julius@skirsch.de>
2025-12-11 23:19:53 +0100

    build: fix security vulnerability

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,26 +5,26 @@ import * as NavigationBar from "expo-navigation-bar";
 import { darkTheme, lightTheme } from "@/assets/colors/colors";
 import { useEffect } from "react";
 import { ThemeProvider } from "@/contexts/themeContext";
-import { DataProvider, useData } from "@/contexts/dataProvider";
-import * as Notifications from "expo-notifications";
+import { DataProvider } from "@/contexts/dataProvider";
 
 export default function RootLayout() {
-    const colorScheme = useColorScheme();
-    const background = colorScheme == "light" ? lightTheme.background : darkTheme.background;
+  const colorScheme = useColorScheme();
+  const background =
+    colorScheme == "light" ? lightTheme.background : darkTheme.background;
 
-    useEffect(() => {
-        NavigationBar.setBackgroundColorAsync(background);
-        StatusBar.setStatusBarBackgroundColor(background);
-    }, [colorScheme]);
+  useEffect(() => {
+    NavigationBar.setBackgroundColorAsync(background);
+    StatusBar.setStatusBarBackgroundColor(background);
+  }, [colorScheme]);
 
-    return (
-        <ThemeProvider>
-            <DataProvider>
-                <Stack>
-                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                    <Stack.Screen name="+not-found" />
-                </Stack>
-            </DataProvider>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider>
+      <DataProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </DataProvider>
+    </ThemeProvider>
+  );
 }
