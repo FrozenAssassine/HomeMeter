201b237d7fff97b3979b89d21c9f489faacc1512
Julius Kirsch <Julius@skirsch.de>
2025-12-23 10:07:53 +0100

    fix: conditional rendering not working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,134 +9,151 @@ import React from "react";
 import { View } from "react-native";
 
 export default function SolarOverview() {
-    const data = useData();
+  const data = useData();
 
-    const historyData = data.solarHistoryData[data.solarHistoryData.length - 1];
+  const historyData = data.solarHistoryData[data.solarHistoryData.length - 1];
 
-    const calculateTotalExported = () => {
-        let total = 0;
-        data.solarHistoryData?.forEach((item) => {
-            if (item.ExportedWH === undefined) return;
-            total += item.ExportedWH;
-        });
-        return total;
-    };
-    const calculateTotalConsumed = () => {
-        let total = 0;
-        data.solarHistoryData?.forEach((item) => {
-            if (item.ConsumedWH === undefined) return;
-            total += item.ConsumedWH;
-        });
-        return total;
-    };
+  const calculateTotalExported = () => {
+    let total = 0;
+    data.solarHistoryData?.forEach((item) => {
+      if (item.ExportedWH === undefined) return;
+      total += item.ExportedWH;
+    });
+    return total;
+  };
+  const calculateTotalConsumed = () => {
+    let total = 0;
+    data.solarHistoryData?.forEach((item) => {
+      if (item.ConsumedWH === undefined) return;
+      total += item.ConsumedWH;
+    });
+    return total;
+  };
 
-    const calculateTotalSolarUsed = () => {
-        let total = 0;
-        data.solarHistoryData?.forEach((item) => {
-            if (item.SelfUsedWH === undefined) return;
-            total += item.SelfUsedWH;
-        });
-        return total;
-    };
+  const calculateTotalSolarUsed = () => {
+    let total = 0;
+    data.solarHistoryData?.forEach((item) => {
+      if (item.SelfUsedWH === undefined) return;
+      total += item.SelfUsedWH;
+    });
+    return total;
+  };
 
-    return (
-        <SimplePage headline="Übersicht" enableScroll={true}>
-            <VerticalView style={{ gap: 10, alignItems: "center", justifyContent: "center" }}>
-                <HomePageDataOverview
-                    leftTitle={(data.liveSolarData?.total.Power.v.toFixed(2) ?? "") + data.liveSolarData?.total.Power.u}
-                    rightTitle={
-                        ((data.livePowerData?.total_power ?? 0) <= 0 ? 0 : data.livePowerData?.total_power.toFixed(2)) +
-                        "W"
-                    }
-                    bottomTitle={
-                        ((data.livePowerData?.total_power ?? 0) + (data.liveSolarData?.total.Power.v ?? 0)).toFixed(2) +
-                        "W"
-                    }
-                    leftIcon="sunny"
-                    rightIcon="flash"
-                    bottomIcon="home"
-                    lineText={
-                        (data.livePowerData?.total_power ?? 0 < 0 ? -1 * (data.livePowerData?.total_power ?? 0) : "0") +
-                        "W"
-                    }
-                    useSolarPower={(data.liveSolarData?.total.Power.v ?? 0) > 0}
-                    useGridPower={(data.livePowerData?.total_power ?? 0) > 0}
-                />
-                <View style={{ marginTop: 10 }} />
-                <SectionHeadline text="Allgemein" />
-                <SingleValueBox
-                    height={30}
-                    headline="Ertrag Heute"
-                    value={
-                        (data.liveSolarData?.total.YieldDay.v.toFixed(0) ?? "") + data.liveSolarData?.total.YieldDay.u
-                    }
-                />
+  return (
+    <SimplePage headline="Übersicht" enableScroll={true}>
+      <VerticalView
+        style={{ gap: 10, alignItems: "center", justifyContent: "center" }}
+      >
+        <HomePageDataOverview
+          leftTitle={
+            (data.liveSolarData?.total.Power.v.toFixed(2) ?? "") +
+            data.liveSolarData?.total.Power.u
+          }
+          rightTitle={
+            ((data.livePowerData?.total_power ?? 0) <= 0
+              ? 0
+              : data.livePowerData?.total_power.toFixed(2)) + "W"
+          }
+          bottomTitle={
+            (
+              (data.livePowerData?.total_power ?? 0) +
+              (data.liveSolarData?.total.Power.v ?? 0)
+            ).toFixed(2) + "W"
+          }
+          leftIcon="sunny"
+          rightIcon="flash"
+          bottomIcon="home"
+          lineText={
+            (data.livePowerData?.total_power ?? 0 < 0
+              ? -1 * (data.livePowerData?.total_power ?? 0)
+              : "0") + "W"
+          }
+          useSolarPower={(data.liveSolarData?.total.Power.v ?? 0) > 0}
+          useGridPower={(data.livePowerData?.total_power ?? 0) > 0}
+        />
+        <View style={{ marginTop: 10 }} />
+        <SectionHeadline text="Allgemein" />
+        <SingleValueBox
+          height={30}
+          headline="Ertrag Heute"
+          value={
+            (data.liveSolarData?.total.YieldDay.v.toFixed(0) ?? "") +
+            data.liveSolarData?.total.YieldDay.u
+          }
+        />
 
-                <SingleValueBox
-                    height={30}
-                    headline="Gesamtertrag"
-                    value={
-                        (data.liveSolarData?.total.YieldTotal.v.toFixed(1) ?? "") +
-                        data.liveSolarData?.total.YieldTotal.u
-                    }
-                />
-                <SingleValueBox height={30} headline="Peak" value={(historyData?.HighestWatt.toFixed(1) ?? "") + "W"} />
+        <SingleValueBox
+          height={30}
+          headline="Gesamtertrag"
+          value={
+            (data.liveSolarData?.total.YieldTotal.v.toFixed(1) ?? "") +
+            data.liveSolarData?.total.YieldTotal.u
+          }
+        />
+        <SingleValueBox
+          height={30}
+          headline="Peak"
+          value={(historyData?.HighestWatt.toFixed(1) ?? "") + "W"}
+        />
 
-                <View style={{ marginTop: 10 }} />
-                {historyData?.ConsumedWH &&
-                    historyData.ExportedWH &&
-                    historyData.SelfConsumptionRatio &&
-                    historyData.SelfUsedWH &&
-                    historyData.AutarkyRatio && (
-                        <>
-                            <SingleValueBox
-                                height={30}
-                                headline={"Hausverbrauch"}
-                                value={historyData.ConsumedWH.toFixed(0) + " Wh"}
-                            />
-                            <SingleValueBox
-                                height={30}
-                                headline={"Netzeinspeisung"}
-                                value={historyData.ExportedWH.toFixed(0) + " Wh"}
-                            />
-                            <SingleValueBox
-                                height={30}
-                                headline={"Umgesetzter Solarstrom"}
-                                value={historyData.SelfUsedWH.toFixed(0) + " Wh"}
-                            />
-                            <SingleValueBox
-                                height={30}
-                                headline={"Eigenverbrauchsquote"}
-                                value={(historyData.SelfConsumptionRatio * 100).toFixed(0) + " %"}
-                            />
-                            <SingleValueBox
-                                height={30}
-                                headline={"Autarkiegrad"}
-                                value={(historyData.AutarkyRatio * 100).toFixed(0) + " %"}
-                            />
-                            <View style={{ marginTop: 10 }} />
+        <View style={{ marginTop: 10 }} />
+        {historyData &&
+          historyData.ConsumedWH !== undefined &&
+          historyData.ExportedWH !== undefined &&
+          historyData.SelfConsumptionRatio !== undefined &&
+          historyData.SelfUsedWH !== undefined &&
+          historyData.AutarkyRatio !== undefined && (
+            <>
+              <SingleValueBox
+                height={30}
+                headline={"Hausverbrauch"}
+                value={historyData.ConsumedWH.toFixed(0) + " Wh"}
+              />
+              <SingleValueBox
+                height={30}
+                headline={"Netzeinspeisung"}
+                value={historyData.ExportedWH.toFixed(0) + " Wh"}
+              />
+              <SingleValueBox
+                height={30}
+                headline={"Umgesetzter Solarstrom"}
+                value={historyData.SelfUsedWH.toFixed(0) + " Wh"}
+              />
+              <SingleValueBox
+                height={30}
+                headline={"Eigenverbrauchsquote"}
+                value={
+                  (historyData.SelfConsumptionRatio * 100).toFixed(0) + " %"
+                }
+              />
+              <SingleValueBox
+                height={30}
+                headline={"Autarkiegrad"}
+                value={(historyData.AutarkyRatio * 100).toFixed(0) + " %"}
+              />
+              <View style={{ marginTop: 10 }} />
 
-                            <SingleValueBox
-                                height={30}
-                                headline={"Gesamt Stromverbrauch"}
-                                value={(calculateTotalConsumed() / 1000).toFixed(2) + "kWh"}
-                            />
-                            <SingleValueBox
-                                height={30}
-                                headline={"Gesamt Solar verwendet"}
-                                value={(calculateTotalSolarUsed() / 1000).toFixed(2) + "kWh"}
-                            />
-                            <SingleValueBox
-                                height={30}
-                                headline={"Gesamt ins Netz"}
-                                value={(calculateTotalExported() / 1000).toFixed(2) + " kWh"}
-                            />
-                        </>
-                    )}
-                <View style={{ marginTop: 10 }} />
-                <SectionHeadline text="Ertrag Zellen" />
-                <IndividualCellDisplay />
-                {/* <SectionHeadline text="Verbrauch" />
+              <SingleValueBox
+                height={30}
+                headline={"Gesamt Stromverbrauch"}
+                value={(calculateTotalConsumed() / 1000).toFixed(2) + "kWh"}
+              />
+              <SingleValueBox
+                height={30}
+                headline={"Gesamt Solar verwendet"}
+                value={(calculateTotalSolarUsed() / 1000).toFixed(2) + "kWh"}
+              />
+              <SingleValueBox
+                height={30}
+                headline={"Gesamt ins Netz"}
+                value={(calculateTotalExported() / 1000).toFixed(2) + " kWh"}
+              />
+            </>
+          )}
+        <View style={{ marginTop: 10 }} />
+        <SectionHeadline text="Ertrag Zellen" />
+        <IndividualCellDisplay />
+        {/* <SectionHeadline text="Verbrauch" />
                 <SingleValueBox
                     headline="Hausverbrauch"
                     value={
@@ -162,7 +179,7 @@ export default function SolarOverview() {
                         ) + "W"
                     }
                 /> */}
-                {/* 
+        {/* 
                 <View
                     style={{
                         width: "100%",
@@ -207,7 +224,7 @@ export default function SolarOverview() {
                         />{" "}
                     </View>
                 </View> */}
-            </VerticalView>
-        </SimplePage>
-    );
+      </VerticalView>
+    </SimplePage>
+  );
 }
--- components/singleValueBox.tsx
+++ components/singleValueBox.tsx
@@ -5,59 +5,61 @@ import SimpleText from "./simpleText";
 import { useColors } from "@/assets/colors/colors";
 
 interface SingleValueBoxProps {
-    headline?: string;
-    value?: string;
-    selectable?: boolean;
-    fontStyle?: TextStyle;
-    accent?: boolean;
-    boxColor?: string;
-    height?: number;
+  headline?: string;
+  value?: string;
+  selectable?: boolean;
+  fontStyle?: TextStyle;
+  accent?: boolean;
+  boxColor?: string;
+  height?: number;
 }
 
 const SingleValueBox: React.FC<SingleValueBoxProps> = ({
-    boxColor,
-    accent,
-    fontStyle,
-    value,
-    headline,
-    selectable,
-    height = 40,
+  boxColor,
+  accent,
+  fontStyle,
+  value,
+  headline,
+  selectable,
+  height = 40,
 }) => {
-    const colors = useColors();
-    return (
-        <SimpleBox
-            style={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "space-between",
-                flexDirection: "row",
-                alignItems: "center",
-                backgroundColor: boxColor ?? "transparent",
-                height: height,
-                padding: 0,
-            }}
-        >
-            <SimpleText style={{ color: accent ? colors.text : colors.accent, fontWeight: "bold" }} fontsize={18}>
-                {headline}
-            </SimpleText>
-            <SimpleText
-                fontsize={22}
-                selectable={selectable}
-                style={[
-                    {
-                        fontWeight: "900",
-                    },
-                    fontStyle,
-                ]}
-            >
-                {value}
-            </SimpleText>
-        </SimpleBox>
-    );
+  const colors = useColors();
+  return (
+    <SimpleBox
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: boxColor ?? "transparent",
+        height: height,
+        padding: 0,
+      }}
+    >
+      <SimpleText
+        style={{
+          color: accent ? colors.text : colors.accent,
+          fontWeight: "bold",
+        }}
+        fontsize={18}
+      >
+        {headline}
+      </SimpleText>
+      <SimpleText
+        fontsize={22}
+        selectable={selectable}
+        style={[
+          {
+            fontWeight: "900",
+          },
+          fontStyle,
+        ]}
+      >
+        {value}
+      </SimpleText>
+    </SimpleBox>
+  );
 };
-
-const styles = StyleSheet.create({
-    text: {},
-});
 
 export default SingleValueBox;
